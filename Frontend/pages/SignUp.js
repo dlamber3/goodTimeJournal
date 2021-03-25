31e575453228accd2ca3f6b2fb9f75181f9719c3
David Lambertson <dlamber3@mail.umw.edu>
2021-03-25 19:50:08 -0400

    Formatted index.js and signup.js and added the frontend and backend folders

---
+++ Frontend/pages/SignUp.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+export default function SignUp() {
+  return (
+    <div
+      style={{
+        margin: "auto auto",
+        width: "800px",
+        textAlign: "center",
+        background: "#daf6db",
+        borderStyle: "groove",
+      }}
+    >
+      <h1>GoodTime, Sign Up</h1>
+      <div>
+        <p> Enter Email Information: </p>
+        <input type="text" id="fname" name="fname" />
+
+        <p> Enter Password: </p>
+        <input type="text" id="fname" name="fname" />
+
+        <p> Enter Name: </p>
+        <input type="text" id="fname" name="fname" />
+        <br></br>
+        <br></br>
+
+        <Link href="/SignUp">
+          <button>Confirm Sign Up</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
---
+++ Frontend/pages/index.js
@@ -0,0 +1,23 @@
+//[label /pages/index.js]
+import Link from 'next/link';
+import React, { Component } from 'react'
+const linkStyle = {
+  marginRight: 15
+}
+
+export default class extends Component {
+  render () {
+    return (
+      <div>
+        <div>Good Time Journal HomeScreen</div>
+        <div>
+          <Link href="/SignUp">
+            <button>Sign Up</button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
+
+
